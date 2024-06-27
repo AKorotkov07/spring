d701bfefa169e375ee690ee4a7bd3ddd1f43d84c
AKorotkov07 <153372700+AKorotkov07@users.noreply.github.com>
2024-06-27 13:13:02 +0300

    Add files via upload

---
+++ java/ru/netology/springbootdemo/ProductionProfile.java
@@ -0,0 +1,7 @@
+package ru.netology.springbootdemo;
+public class ProductionProfile implements SystemProfile {
+    @Override
+    public String getProfile() {
+        return "Current profile is production";
+    }
+}
